be8e3eeffa35009d62847a153532d38fd5f0159f
nicholasthomas18 <nt325@byu.edu>
2025-11-23 22:47:13 -0700

    update walmart

--- walmart-recruiting-store-sales-forecasting/Walmart.R
+++ walmart-recruiting-store-sales-forecasting/Walmart.R
@@ -3,6 +3,7 @@ library(tidyverse)
 library(vroom)
 library(tidymodels)
 library(DataExplorer)
+library(lubridate)
 
 ## Read in the Data
 train <- vroom("/Users/nicholasthomas/Desktop/STATISTICS/STAT 348/Walmart/walmart-recruiting-store-sales-forecasting/train.csv")
@@ -84,5 +85,99 @@ feature_recipe <- recipe(~., data=features) %>%
 imputed_features <- juice(prep(feature_recipe))
 
 
-view(imputed_features)
-view(train)
+
+train_full <- train %>%
+  left_join(imputed_features %>% select(-IsHoliday), by = c("Store", "Date")) %>%
+  left_join(stores, by = "Store")
+train_full <- train_full %>% drop_na(Weekly_Sales)
+
+
+sample_store_depts <- train_full %>%
+  distinct(Store, Dept) %>%
+  slice_sample(n = 20)   # change 20 to whatever sample size you want
+
+train_sample <- train_full %>%
+  semi_join(sample_store_depts, by = c("Store", "Dept"))
+
+
+
+sales_rec <- recipe(Weekly_Sales ~ ., data = train_sample) %>%
+  step_rm(Date) %>%
+  step_mutate(
+    Store = factor(Store),
+    Dept  = factor(Dept),
+    Type  = factor(Type)
+  ) %>%
+  step_dummy(all_nominal_predictors()) %>%
+  step_zv(all_predictors()) %>%
+  step_normalize(all_numeric_predictors(), -all_outcomes())
+
+sales_prep <- prep(sales_rec)
+
+train_baked <- bake(sales_prep, new_data = NULL)
+
+n_pred <- train_baked %>%
+  select(-Weekly_Sales) %>%
+  ncol()
+
+
+folds <- vfold_cv(train_sample, v = 5)
+
+
+# Linear Model
+lm_spec <- linear_reg() %>%
+  set_engine("lm")
+
+lm_wf <- workflow() %>%
+  add_model(lm_spec) %>%
+  add_recipe(sales_rec)
+
+time_lm <- system.time({
+  lm_res <- fit_resamples(
+    lm_wf,
+    resamples = folds,
+    metrics   = metric_set(rmse, rsq),
+    control   = control_resamples(save_pred = TRUE)
+  )
+})
+
+lm_metrics <- lm_res %>% collect_metrics()
+lm_metrics
+
+# Random Forest
+rf_spec <- rand_forest(
+  mtry  = tune(),
+  min_n = tune(),
+  trees = 500
+) %>%
+  set_engine("ranger") %>%
+  set_mode("regression")
+
+rf_wf <- workflow() %>%
+  add_model(rf_spec) %>%
+  add_recipe(sales_rec)
+
+rf_params <- parameters(
+  mtry(range = c(1L, n_pred)),
+  min_n(range = c(2L, 50L))   # adjust upper bound if you want
+)
+
+rf_grid <- grid_space_filling(rf_params, size = 20)
+
+time_rf <- system.time({
+  rf_res <- tune_grid(
+    rf_wf,
+    resamples = folds,
+    grid      = rf_grid,
+    metrics   = metric_set(rmse, rsq),
+    control   = control_grid(save_pred = TRUE)
+  )
+})
+
+rf_best <- rf_res %>% tune::select_best(metric = "rmse")
+
+rf_metrics <- rf_res %>%
+  collect_metrics() %>%
+  filter(.config == rf_best$.config)
+
+rf_metrics
